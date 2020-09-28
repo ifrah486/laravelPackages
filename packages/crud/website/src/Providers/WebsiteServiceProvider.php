2cc64448dd3fc253825a0793f83ad0815b26e0c3
@ifrahifii <ayaaat4567@gmail.com>
2020-09-28 13:41:50 +0500

    first commit

---
+++ packages/crud/website/src/Providers/WebsiteServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace crud\website\Providers;
+use Illuminate\Support\ServiceProvider;
+class WebsiteServiceProvider extends ServiceProvider
+{
+
+ public function boot()
+ {
+     dd('welcome in packages');
+ }
+
+ public function register()
+ {
+
+
+ }
+
+}
